f052437568bffd7c94079b5cc1ea1458854b64cd
MNM YOUNUS <mnmyounus@gmail.com>
2026-03-21 00:16:03 +0530

    Update MediaFragment.kt

--- app/src/main/java/com/mnmyounus/ymr/ui/features/MediaFragment.kt
+++ app/src/main/java/com/mnmyounus/ymr/ui/features/MediaFragment.kt
@@ -1,25 +1,32 @@
 package com.mnmyounus.ymr.ui.features
-import android.os.Bundle; import android.view.*
-import androidx.fragment.app.Fragment; import androidx.viewpager2.adapter.FragmentStateAdapter
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import com.mnmyounus.ymr.databinding.FragmentFeaturesBinding
+import androidx.viewpager2.widget.ViewPager2
+import com.mnmyounus.ymr.R
 
 class MediaFragment : Fragment() {
-    private var _b: FragmentFeaturesBinding? = null
-    private val b get() = _b!!
-    override fun onCreateView(i: LayoutInflater, c: ViewGroup?, s: Bundle?): View {
-        _b = FragmentFeaturesBinding.inflate(i, c, false); return b.root
-    }
+
+    override fun onCreateView(i: LayoutInflater, c: ViewGroup?, s: Bundle?): View =
+        i.inflate(R.layout.fragment_media_tabs, c, false)
+
     override fun onViewCreated(v: View, s: Bundle?) {
         super.onViewCreated(v, s)
-        // Hide chips (not needed in media context)
-        b.chipGroupApps.visibility = View.GONE
         val tabs = listOf("Photos" to "photos", "Videos" to "videos", "Audio" to "audio", "Docs" to "docs")
-        b.viewPager.adapter = object : FragmentStateAdapter(this) {
+        val vp = v.findViewById<ViewPager2>(R.id.mediaViewPager)
+        val tl = v.findViewById<TabLayout>(R.id.mediaTabLayout)
+
+        vp.adapter = object : FragmentStateAdapter(this) {
             override fun getItemCount() = tabs.size
             override fun createFragment(pos: Int) = MediaListFragment.newInstance(tabs[pos].second)
         }
-        TabLayoutMediator(b.tabLayout, b.viewPager) { tab, pos -> tab.text = tabs[pos].first }.attach()
+        vp.offscreenPageLimit = 1
+
+        TabLayoutMediator(tl, vp) { tab, pos -> tab.text = tabs[pos].first }.attach()
     }
-    override fun onDestroyView() { super.onDestroyView(); _b = null }
 }
